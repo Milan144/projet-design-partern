5455c0fd175ba8bb91466dbed00078087935d4b7
Theo-max-hue <theo.gamard@orange.fr>
2022-10-06 14:54:31 +0200

    modif class

--- class/class.dataretriever.php
+++ class/class.dataretriever.php
@@ -1,22 +1,26 @@
 <?php
 
-class DataRetriever{
+class DataRetriever
+{
     private $url;
     private $data = array();
 
-    public function __construct($url){
+    public function __construct($url)
+    {
         $this->url = $url;
     }
 
-    public function getData(){
+    public function getData()
+    {
         $json = file_get_contents($this->url);
         $this->data = json_decode($json, true);
         return $this->data;
     }
 
-    public function getPairPrice($pair){
+    public function getPairPrice($pair)
+    {
         foreach ($this->data as $key => $onePair) {
-            if($onePair["symbol"] == $pair){
+            if ($onePair["symbol"] == $pair) {
                 return $onePair['price'];
             }
         }
