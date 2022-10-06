9c84f95ac759ab6c307c9ce62fb47dc7eebaf1a0
milan <milan.hommet@protonmail.com>
2022-10-06 16:54:04 +0200

    ntm mysql

--- bot-crypto.sql
+++ bot-crypto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Oct 06, 2022 at 02:39 PM
+-- Generation Time: Oct 06, 2022 at 02:51 PM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
