6ded48baa5ee90f8e605e8dafbc664e6a810ddb2
Theo-max-hue <theo.gamard@orange.fr>
2022-10-06 16:42:03 +0200

    wallet fix

--- main.php
+++ main.php
@@ -3,6 +3,7 @@ include("connexion.php");
 include("class/class.proxydataretriever.php");
 include("class/class.dataretriever.php");
 include("class/class.algorithm.php");
+include("class/class.wallet.php");
 
 
 // $urlAPI = "https://api.binance.com/api/v3/ticker/price";
@@ -19,5 +20,5 @@ include("class/class.algorithm.php");
 
 
 $manager = new WalletManager($pdo);
-$wallet = $manager->getById(2);
+$wallet = $manager->getById(1);
 var_dump($wallet);
