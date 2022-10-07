1ed6ea936a2e15dcad8b268e1ac4fa40aca45bb8
milan <milan.hommet@protonmail.com>
2022-10-07 12:07:44 +0200

    test singleton

--- connexion.php
+++ connexion.php
@@ -4,3 +4,23 @@ try {
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
+
+
+//Singleton
+Class DBConnector{
+
+    static Connection mysql;
+
+    public static Connection getConnection(){
+        if(mysql == null){
+            mysql = new MySqlInstance(
+                            "localhost" ,
+                            "root" ,
+                            "" ,
+                            "bot-crypto"
+                        );
+        } 
+        return mysql;
+    }
+
+}
